274a73a75148b453a278a9d225e69d08477de87f
mcowden808 <mcowden808@yahoo.com>
2019-10-15 13:53:24 -0700

    react fetch added

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,26 @@
+/* eslint-disable no-console */
 import React from "react";
 
+const fetch = require("node-fetch");
+
 class App extends React.Component {
+  // eslint-disable-next-line class-methods-use-this
+  componentDidMount() {
+    fetch("/definition/word", {
+      method: "POST",
+      body: JSON.stringify({ word: "test" }),
+      headers: {
+        "Content-Type": "application/json"
+      }
+    })
+      .then(data => {
+        // console.log(data);
+        return data.json();
+      })
+      .then(({ definitionQuery }) => {
+        console.log(definitionQuery[0].definition);
+      });
+  }
   // eslint-disable-next-line
   render() {
     return <div>React loaded!!!!</div>;
