5a8c3f52804769da501f546867266c72b88afc5d
mcowden808 <mcowden808@yahoo.com>
2019-10-16 13:54:34 -0700

    circleci job test

--- server/db/schema.sql
+++ server/db/schema.sql
@@ -1,6 +1,6 @@
 /*  Execute this file from the command line by typing:
  *    mysql -u <USER> < schema.sql
- *    OR:
+ *    OR
  *    mysql -u <USER> -p < schema.sql
  *  For example, on a pairing station, it'll be
  *    mysql -u student -p < schema.sql
