447cfac59a2c41d9c11ac6097df673ce576a9065
mcowden808 <mcowden808@yahoo.com>
2019-10-18 12:56:12 -0700

    added initial mug advertisement

--- client/src/components/Definition.jsx
+++ client/src/components/Definition.jsx
@@ -18,24 +18,39 @@ class Definition extends React.Component {
     };
   }
   render() {
-    console.log(this.props.def_id)
-    const hashed = this.state.hashtags.split(" ").map(item => <span className="hash_green">{"#" + item + " "}</span>);
+    console.log(this.props.def_id);
+    const hashed = this.state.hashtags
+      .split(" ")
+      .map(item => <span className="hash_green">{"#" + item + " "}</span>);
     return (
       <div className="defs">
-
-        {this.props.def_id === 1 ? <div className="def-count">top definition</div> : <div className="def-count">{this.props.def_id}</div>}
-        <div className="word-title">Word</div>
-        <p>{this.state.definition}</p>
+        {this.props.def_id === 1 ? (
+          <div className="def-count">top definition</div>
+        ) : (
+          <div className="def-count">{this.props.def_id}</div>
+        )}
+        <div className="word_title">Word</div>
+        <p className="single_def">{this.state.definition}</p>
         <p className="example">{this.state.example}</p>
         <div className="hash">{hashed}</div>
         <div className="buttonContainer">
-        <VoteButtonContainer
-          upvotes={this.state.upVotes}
-          downvotes={this.state.downVotes}
-        />
+          <VoteButtonContainer
+            upvotes={this.state.upVotes}
+            downvotes={this.state.downVotes}
+          />
         </div>
         <div>
-          by <span className="author">{this.state.createdBy}</span> {this.state.createdDate}
+          by <span className="author">{this.state.createdBy}</span>{" "}
+          {this.state.createdDate}
+        </div>
+        <div className="tone">
+          <div className="mug-text">Get a <span className="mug-word">Word</span> mug for your cousin Steve.</div>
+          <div>
+            <img
+              className="mug-image"
+              src="https://d2gatte9o95jao.cloudfront.net/assets/mug-ad-02@2x-5e65e4fe0daf91156b197594c58445606b861fc9317ca6c02fad7b75dfb58e22.png"
+            ></img>
+          </div>
         </div>
       </div>
     );
