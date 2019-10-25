3e596d0c438a1fee0bb5310e5fb8774aa60844bb
mcowden808 <mcowden808@yahoo.com>
2019-10-24 19:53:13 -0700

    removed body parser, added mysql shortcut commands, cleaned up circleci yml

--- server/server.js
+++ server/server.js
@@ -4,7 +4,6 @@ import ReactDOM from "react-dom/server";
 import App from "../client/src/App.jsx";
 
 const express = require("express");
-const bodyParser = require("body-parser");
 const path = require("path");
 const { queryWordData } = require("../server/models/queryWordData.js");
 
@@ -12,8 +11,6 @@ const app = express();
 const port = process.env.PORT || 8001;
 const processArgs = process.argv.slice(2);
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, "/../public")));
 app.get("/:word", (req, res) => {
   queryWordData(req.params.word, (error, wordData) => {
